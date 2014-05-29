6c8ca685bb9cdc07a2f278fcf37473cc8bb420f1
lsmith17 <lsmith17@my.whitworth.edu>
2014-05-28 19:41:56 -0700

    Fixed Code

--- ex03_01/ex03_01/Fan.cpp
+++ ex03_01/ex03_01/Fan.cpp
@@ -4,39 +4,59 @@ using namespace std;
 class Fan {
 private:
 	int speed;
-public:
-	int GetSpeed(){
-		return speed;
-	}
-	void SetSpeed(int new_speed){
-		if (new_speed == 1 || 2 || 3)
-			speed = new_speed;
-	}
 	double radius;
 	bool on;
-
+public:
+	
 	Fan(){
 		speed = 1;
 		radius = 5;
 		on = false;
 	}
-	Fan(int s, double r, bool o = false){
-		s = speed;
-		r = radius;
-		o = on;
+	int getSpeed(){
+		return speed;
 	}
+	void setSpeed(int new_speed){
+		//if (new_speed == 1 || 2 || 3){
+			speed = new_speed;
+		//}
+		//else{
+			//cout << "Give a 1, 2 or 3" << endl;
+		//}
 		
+	}
+	double getRadius(){
+		return radius;
+	}
+	void setRadius(double new_radius){
+		radius = new_radius;
+	}
+	bool status(){
+		return on;
+	}
+	void setOn(bool OnOrOff){
+		on = OnOrOff;
+	}	
 
 };
 
 int main(){
-	Fan F1(3, 10, true);
-	Fan F2(2, 5);
 
-	cout << "First Fan Properties: " << endl; 
-	cout << "Speed: " << F1.speed << "  Radius: " << F1.radius << "  Status: " << F1.on << endl << endl;
+	Fan F1;
+	F1.setSpeed(3);
+	F1.setRadius(10);
+	F1.setOn(true);
+	
+	cout << "First Fan Properties: " << endl;
+	cout << "Speed: " << F1.getSpeed() << "  Radius: " << F1.getRadius() << "  Status: " << F1.status() << endl << endl;
+	
+	Fan F2;
+	F2.setSpeed(2);
+	F2.setRadius(5);
+	F2.setOn(false);
+	
 	cout << "Second Fan Properties: " << endl;
-	cout << "Speed: " << F2.speed << "  Radius: " << F2.radius << "  Status: " << F2.on << endl;
+	cout << "Speed: " << F2.getSpeed() << "  Radius: " << F2.getRadius() << "  Status: " << F2.status() << endl;
 
 	return 0;
 }
